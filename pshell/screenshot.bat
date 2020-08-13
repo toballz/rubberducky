9a6bf4aa14088e506257d501b871c3d3160739e4
toballz <42551340+toballz@users.noreply.github.com>
2020-08-13 12:25:28 -0400

    Create screenshot.bat

---
+++ pshell/screenshot.bat
@@ -0,0 +1,19 @@
+Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+
+$screens = [Windows.Forms.Screen]::AllScreens
+
+$top    = ($screens.Bounds.Top    | Measure-Object -Minimum).Minimum
+$left   = ($screens.Bounds.Left   | Measure-Object -Minimum).Minimum
+$width  = ($screens.Bounds.Right  | Measure-Object -Maximum).Maximum
+$height = ($screens.Bounds.Bottom | Measure-Object -Maximum).Maximum
+
+$bounds   = [Drawing.Rectangle]::FromLTRB($left, $top, $width, $height)
+$bmp      = New-Object System.Drawing.Bitmap ([int]$bounds.width), ([int]$bounds.height)
+$graphics = [Drawing.Graphics]::FromImage($bmp)
+
+$graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+$bmp.Save("C:/xampp/htdocs/webhost/server_admin/directIP\test.png")
+
+$graphics.Dispose()
+$bmp.Dispose()
