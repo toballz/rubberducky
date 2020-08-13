5e8b7cd372f01d6c6f3feee892dc2060d6863d99
toballz <42551340+toballz@users.noreply.github.com>
2020-08-13 13:23:42 -0400

    Update screenshot.bat

--- pshell/screenshot.bat
+++ pshell/screenshot.bat
@@ -1,19 +1,10 @@
-Add-Type -AssemblyName System.Windows.Forms,System.Drawing
+Add-Type -AssemblyName System.Windows.Forms,System.Drawing;
 
-$screens = [Windows.Forms.Screen]::AllScreens
+$screens = [Windows.Forms.Screen]::AllScreens;$top = ($screens.Bounds.Top | Measure-Object -Minimum).Minimum;$left = ($screens.Bounds.Left | Measure-Object -Minimum).Minimum;$width = ($screens.Bounds.Right | Measure-Object -Maximum).Maximum;$height = ($screens.Bounds.Bottom | Measure-Object -Maximum).Maximum;
+$bounds = [Drawing.Rectangle]::FromLTRB($left, $top, $width, $height);$bmp = New-Object System.Drawing.Bitmap ([int]$bounds.width), ([int]$bounds.height);
 
-$top    = ($screens.Bounds.Top    | Measure-Object -Minimum).Minimum
-$left   = ($screens.Bounds.Left   | Measure-Object -Minimum).Minimum
-$width  = ($screens.Bounds.Right  | Measure-Object -Maximum).Maximum
-$height = ($screens.Bounds.Bottom | Measure-Object -Maximum).Maximum
-
-$bounds   = [Drawing.Rectangle]::FromLTRB($left, $top, $width, $height)
-$bmp      = New-Object System.Drawing.Bitmap ([int]$bounds.width), ([int]$bounds.height)
-$graphics = [Drawing.Graphics]::FromImage($bmp)
-
-$graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
-
-$bmp.Save("$ENV:UserProfile/Pictures/test.png")
-
-$graphics.Dispose()
-$bmp.Dispose()
+$graphics = [Drawing.Graphics]::FromImage($bmp);
+$graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size);
+$bmp.Save($ENV:UserProfile+"/Pictures/test.png");
+$graphics.Dispose();
+$bmp.Dispose();
